6a592613b1b22eb6d60d501d9ea778f27db29ae0
Bambusek Daniel <bambusek7619@gmail.com>
2019-08-20 18:10:30 +0200

    mixedreality toolkit added as submodule

--- link_submodules.bat
+++ link_submodules.bat
@@ -1,2 +1,3 @@
 mklink /j .\arcor2_AREditor\Assets\Submodules\JSONObject .\arcor2_AREditor\Submodules\JSONObject
+mklink /j .\arcor2_AREditor\Assets\Submodules\MixedRealityToolkit .\arcor2_AREditor\Submodules\MixedRealityToolkit-Unity\Assets
 pause
