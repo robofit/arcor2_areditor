cb309b5685a4223eb30dd02f146528eb8ed60f62
Daniel Bambusek <bambusek7619@gmail.com>
2019-09-03 17:36:07 +0200

    bash script for symlinking submodules in unix

---
+++ link_submodules.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir arcor2_AREditor/Assets/Submodules
+ln -s `pwd`/arcor2_AREditor/Submodules/JSONObject arcor2_AREditor/Assets/Submodules/JSONObject
+ln -s `pwd`/arcor2_AREditor/Submodules/MixedRealityToolkit-Unity/Assets arcor2_AREditor/Assets/Submodules/MixedRealityToolkit-Unity
