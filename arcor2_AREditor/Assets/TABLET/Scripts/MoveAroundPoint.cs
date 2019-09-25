91588af3caf95f22664ecbfc0d40488adbf802db
Bambusek Daniel <bambusek7619@gmail.com>
2019-09-25 16:01:42 +0200

    basic AR tablet scene with marker detection using ARFoundation library; debug tools added

---
+++ arcor2_AREditor/Assets/TABLET/Scripts/MoveAroundPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAroundPoint : MonoBehaviour
+{
+    public Transform Center;
+    public Vector3 RotateAroundAxis;
+    public float Speed;
+
+    // Update is called once per frame
+    void Update() {     
+        transform.RotateAround(Center.transform.position, RotateAroundAxis, Speed);
+    }
+}
