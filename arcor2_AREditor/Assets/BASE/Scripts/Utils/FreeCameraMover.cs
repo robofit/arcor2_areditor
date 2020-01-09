b674220860378a87ab2ace63393585ac957fe93f
xBambusekD <bambusek7619@gmail.com>
2020-01-09 16:32:42 +0100

    ConditionalFieldAttributes added + FreeCameraMover updated

--- arcor2_AREditor/Assets/BASE/Scripts/Utils/FreeCameraMover.cs
+++ arcor2_AREditor/Assets/BASE/Scripts/Utils/FreeCameraMover.cs
@@ -1,33 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using MyBox;
 using UnityEngine;
 
 public class FreeCameraMover : MonoBehaviour
 {
-    [SerializeField]
-    private float WalkingSpeed = 5f;
-    [SerializeField]
-    private float CameraRotationSpeed = 0.5f;
+    
+    public bool ControlWithMouseOnly = true;
 
-    private Vector3 mousePosition;
 
-    // Start is called before the first frame update
-    void Start() {
-        mousePosition = Input.mousePosition;
-    }
+    public float CameraRotationSpeed = 4f;
+
+    [ConditionalField("ControlWithMouseOnly", inverse:true)]
+    public float WalkingSpeed = 5f;
+
+    [ConditionalField("ControlWithMouseOnly")]
+    public float zoomSpeed = 2f;
+
+    [ConditionalField("ControlWithMouseOnly")]
+    public float dragSpeed = 4f;
+
+    private float yaw = 0f;
+    private float pitch = 0f;
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.Mouse1)) {
-            mousePosition = Input.mousePosition - mousePosition;
-            mousePosition = new Vector3(-mousePosition.y * CameraRotationSpeed, mousePosition.x * CameraRotationSpeed, 0);
-            mousePosition = new Vector3(transform.eulerAngles.x + mousePosition.x, transform.eulerAngles.y + mousePosition.y, 0);
-            transform.eulerAngles = mousePosition;
-        }
-        mousePosition = Input.mousePosition;
+        //rotate with right mouse
+        if (Input.GetMouseButton(1)) {
+            yaw += CameraRotationSpeed * Input.GetAxis("Mouse X");
+            pitch -= CameraRotationSpeed * Input.GetAxis("Mouse Y");
 
-        Vector3 translation = GetDirection();
-        transform.Translate(translation * Time.deltaTime * WalkingSpeed);        
+            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        }
+
+        if (ControlWithMouseOnly) {
+            //drag camera around with Middle Mouse
+            if (Input.GetMouseButton(2)) {
+                transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * dragSpeed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * dragSpeed, 0);
+            }
+
+            //Zoom in and out with Mouse Wheel
+            transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Space.Self);
+        } else {
+            Vector3 translation = GetDirection();
+            transform.Translate(translation * Time.deltaTime * WalkingSpeed);
+        }
     }
 
     private Vector3 GetDirection() {
