8dbc3679b97d75c7896717e41e388d5ba37be470
xBambusekD <bambusek7619@gmail.com>
2020-01-09 19:38:09 +0100

    Added UnityRuntimeGizmo submodule for moving the objects in runtime.

--- link_submodules.sh
+++ link_submodules.sh
@@ -5,3 +5,4 @@ ln -s `pwd`/arcor2_AREditor/Submodules/JSONObject arcor2_AREditor/Assets/Submodu
 ln -s `pwd`/arcor2_AREditor/Submodules/MixedRealityToolkit-Unity/Assets arcor2_AREditor/Assets/Submodules/MixedRealityToolkit-Unity
 ln -s `pwd`/arcor2_AREditor/Submodules/UnityDynamicPanels/Plugins arcor2_AREditor/Assets/Submodules/UnityDynamicPanels
 ln -s `pwd`/arcor2_AREditor/Submodules/UnityRuntimeInspector/Plugins arcor2_AREditor/Assets/Submodules/UnityRuntimeInspector
+ln -s `pwd`/arcor2_AREditor/Submodules/Unity3DRuntimeTransformGizmo/Assets arcor2_AREditor/Assets/Submodules/Unity3DRuntimeTransformGizmo
