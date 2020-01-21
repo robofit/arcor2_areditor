b554a493e51b915210f7f7e63b79d05a95ea41ec
xBambusekD <bambusek7619@gmail.com>
2020-01-21 14:21:04 +0100

    UIGraph moved into Submodules

--- arcor2_AREditor/Assets/2D_EDITOR/UIGraph/Scripts/Util/ObfuscateObject.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class ObfuscateObject : MonoBehaviour {
-    [ContextMenu("Obfuscate")]
-    public void Obfuscate() {
-        gameObject.name = gameObject.name.GetHashCode().ToString();
-    }
-}
