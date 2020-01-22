6b8afc65c6f32b9f0bcd4263d495db5177c8bb30
ZdenekM <imaterna@fit.vutbr.cz>
2020-01-22 09:44:21 +0100

    Now it works even if target directory exists.

--- link_submodules.sh
+++ link_submodules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir arcor2_AREditor/Assets/Submodules
+mkdir -p arcor2_AREditor/Assets/Submodules
 ln -s `pwd`/arcor2_AREditor/Submodules/JSONObject arcor2_AREditor/Assets/Submodules/JSONObject
 ln -s `pwd`/arcor2_AREditor/Submodules/MixedRealityToolkit-Unity/Assets arcor2_AREditor/Assets/Submodules/MixedRealityToolkit-Unity
 ln -s `pwd`/arcor2_AREditor/Submodules/UnityDynamicPanels/Plugins arcor2_AREditor/Assets/Submodules/UnityDynamicPanels
