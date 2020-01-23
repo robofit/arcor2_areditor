dfd917962ae0a6f1323460582ca86d80236c8236
Michal Kapinus <michal.kapinus@gmail.com>
2020-01-23 16:57:22 +0100

    automatic loading of default values for dynamic parameters, get rid of SELECTED_ITEM..

--- arcor2_AREditor/Assets/BASE/Scripts/Action.cs
+++ arcor2_AREditor/Assets/BASE/Scripts/Action.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
+using System;
+using System.Threading.Tasks;
 
 namespace Base {
     public abstract class Action : Clickable {
@@ -11,14 +13,16 @@ namespace Base {
         public IActionProvider ActionProvider;
 
         public IO.Swagger.Model.Action Data = new IO.Swagger.Model.Action("", new List<IO.Swagger.Model.ActionIO>(), new List<IO.Swagger.Model.ActionIO>(), new List<IO.Swagger.Model.ActionParameter>(), "");
-        public void Init(string id, ActionMetadata metadata, ActionPoint ap, bool generateData, IActionProvider actionProvider, bool updateProject = true) {
+        public async Task Init(string id, ActionMetadata metadata, ActionPoint ap, bool generateData, IActionProvider actionProvider, bool updateProject = true) {
 
             this.metadata = metadata;
             this.ActionProvider = actionProvider;
 
             if (generateData) {
+                List<ActionParameter> dynamicParameters = new List<ActionParameter>();
                 foreach (IO.Swagger.Model.ActionParameterMeta actionParameterMetadata in this.metadata.Parameters) {
-                    ActionParameter actionParameter = new ActionParameter(actionParameterMetadata);
+                    
+                    ActionParameter actionParameter = new ActionParameter(actionParameterMetadata, this);
                     switch (actionParameter.Type) {
                         case "relative_pose":
                             actionParameter.Value = Regex.Replace(new IO.Swagger.Model.Pose(orientation: new IO.Swagger.Model.Orientation(), position: new IO.Swagger.Model.Position()).ToJson(), @"\t|\n|\r", "");
@@ -43,15 +47,53 @@ namespace Base {
                             break;
 
                     }
+                    if (actionParameterMetadata.DynamicValue) {
+                        dynamicParameters.Add(actionParameter);
+                    }
 
                     Parameters[actionParameter.ActionParameterMetadata.Name] = actionParameter;
                 }
                 foreach (InputOutput io in GetComponentsInChildren<InputOutput>()) {
                     io.InitData();
                 }
+                int parentCount = 0;
+                while (dynamicParameters.Count > 0) {
+                    for (int i = dynamicParameters.Count - 1; i >= 0; i--) {
+                        ActionParameter actionParameter = dynamicParameters[i];
+                        if (actionParameter.ActionParameterMetadata.DynamicValueParents.Count == parentCount) {
+                            try {
+                                List<IO.Swagger.Model.IdValue> args = new List<IO.Swagger.Model.IdValue>();
+                                foreach (string parent in actionParameter.ActionParameterMetadata.DynamicValueParents) {
+                                    string paramValue = "";
+                                    if (parameters.TryGetValue(parent, out ActionParameter parameter)) {
+                                        parameter.GetValue(out paramValue);
+                                    } else {
+                                        //TODO raise exception
+                                    }
+                                    args.Add(new IO.Swagger.Model.IdValue(parent, paramValue));
+                                }
+                                List<string> values = await actionParameter.LoadDynamicValues(args);
+                                if (values.Count > 0) {
+                                    actionParameter.Value = values[0];
+                                } else {
+                                    actionParameter.Value = "";
+                                }
+                            } catch (Exception ex) when (ex is ItemNotFoundException || ex is Base.RequestFailedException) {
+                                Debug.LogError(ex);
+                            } finally {
+                                dynamicParameters.RemoveAt(i);
+                            }
+                        }
+                    }
+                    parentCount += 1;
+                }
             }
+
+            
+
             UpdateId(id, false);
             UpdateType();
+
 
             if (updateProject) {
                 GameManager.Instance.UpdateProject();
@@ -65,6 +107,8 @@ namespace Base {
         public void UpdateType() {
             Data.Type = GetActionType();
         }
+
+        
 
         public virtual void UpdateId(string newId, bool updateProject = true) {
             Data.Id = newId;
