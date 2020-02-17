95a6f2c0bc42e4d365d66679b62809b226bb726f
Michal Kapinus <michal.kapinus@gmail.com>
2020-02-17 17:34:05 +0100

    added script for models building (linux only yet), scripts moved to scripts folder

--- link_submodules.bat
+++
@@ -1,7 +0,0 @@
-md .\arcor2_AREditor\Assets\Submodules
-mklink /j .\arcor2_AREditor\Assets\Submodules\JSONObject .\arcor2_AREditor\Submodules\JSONObject
-mklink /j .\arcor2_AREditor\Assets\Submodules\MixedRealityToolkit .\arcor2_AREditor\Submodules\MixedRealityToolkit-Unity\Assets
-mklink /j .\arcor2_AREditor\Assets\Submodules\UnityDynamicPanels .\arcor2_AREditor\Submodules\UnityDynamicPanels\Plugins
-mklink /j .\arcor2_AREditor\Assets\Submodules\UnityRuntimeInspector .\arcor2_AREditor\Submodules\UnityRuntimeInspector\Plugins
-mklink /j .\arcor2_AREditor\Assets\Submodules\Unity3DRuntimeTransformGizmo .\arcor2_AREditor\Submodules\Unity3DRuntimeTransformGizmo\Assets
-pause
