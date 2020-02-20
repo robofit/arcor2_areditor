da556019a3a2129af963dc569d405fc3866ffb6a
xBambusekD <bambusek7619@gmail.com>
2020-02-20 14:17:06 +0100

    Gizmo cannot be clicked through to any other gameobjects

--- arcor2_AREditor/Assets/TABLET/Scripts/OnClickCollider.cs
+++ arcor2_AREditor/Assets/TABLET/Scripts/OnClickCollider.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using RuntimeGizmos;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class OnClickCollider : MonoBehaviour
 {
     public GameObject Target;
-
+    
     public void OnClick()
     {
         Target.GetComponent<Base.Clickable>().OnClick(Base.Clickable.Click.TOUCH);
@@ -17,6 +18,7 @@ public class OnClickCollider : MonoBehaviour
         if (EventSystem.current.IsPointerOverGameObject()) {
             return;
         }
+
         if (Input.GetMouseButtonDown(0)) {
             Target.GetComponent<Base.Clickable>().OnClick(Base.Clickable.Click.MOUSE_LEFT_BUTTON);
         }
