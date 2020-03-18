b7cf867018d8e16c9ea933dcd23e11153a41c1b3
Michal Kapinus <michal.kapinus@gmail.com>
2020-03-18 09:59:08 +0100

    fix: hamburger menu on main screen could not be opened

--- arcor2_AREditor/Assets/BASE/Scripts/MainsScreen.cs
+++ arcor2_AREditor/Assets/BASE/Scripts/MainsScreen.cs
@@ -17,9 +17,11 @@ public class MainsScreen : MonoBehaviour {
 
     private void ShowEditorWindow(object sender, EventArgs args) {
         CanvasGroup.alpha = 1;
+        CanvasGroup.blocksRaycasts = true;
     }
 
     private void HideEditorWindow(object sender, EventArgs args) {
         CanvasGroup.alpha = 0;
+        CanvasGroup.blocksRaycasts = false;
     }
 }
