19c5e87df8b0a5edcab0d3883485e5ce3026b8bf
Kapim <michal.kapinus@gmail.com>
2020-03-23 15:03:30 +0100

    new models disabled services/objects not interacactable, thus cannot be added to scene

--- arcor2_AREditor/Assets/2D_EDITOR/Scripts/ServiceButton.cs
+++ arcor2_AREditor/Assets/2D_EDITOR/Scripts/ServiceButton.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Michsky.UI.ModernUIPack;
 
 public class ServiceButton : MonoBehaviour
 {
@@ -8,6 +10,15 @@ public class ServiceButton : MonoBehaviour
     public IO.Swagger.Model.ServiceTypeMeta ServiceMetadata;
     public GameObject Yes, No;
     private bool state;
+    [SerializeField]
+    private Button button;
+    [SerializeField]
+    private TooltipContent tooltipContent;
+
+    private void Start() {
+        tooltipContent.tooltipRect = TooltipRef.Instance.Tooltip;
+        tooltipContent.descriptionText = TooltipRef.Instance.Text;
+    }
 
     public bool State {
         get => state;
@@ -20,6 +31,16 @@ public class ServiceButton : MonoBehaviour
                 Yes.SetActive(false);
                 No.SetActive(true);
             }
+        }
+    }
+
+    public void SetInteractable(bool interactable) {
+        button.interactable = interactable;
+        if (!interactable) {
+            tooltipContent.enabled = true;
+            tooltipContent.description = ServiceMetadata.Description + ServiceMetadata.Problem;
+        } else {
+            tooltipContent.enabled = false;
         }
     }
 
