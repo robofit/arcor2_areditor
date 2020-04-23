597ea4ab2da1fa727a5c65cad1388222a60006a3
xBambusekD <bambusek7619@gmail.com>
2020-04-24 00:02:08 +0200

    ROS# integration via git submodule

--- scripts/link_submodules.bat
+++ scripts/link_submodules.bat
@@ -4,4 +4,5 @@ mklink /j ..\arcor2_AREditor\Assets\Submodules\MixedRealityToolkit ..\arcor2_ARE
 mklink /j ..\arcor2_AREditor\Assets\Submodules\UnityDynamicPanels ..\arcor2_AREditor\Submodules\UnityDynamicPanels\Plugins
 mklink /j ..\arcor2_AREditor\Assets\Submodules\UnityRuntimeInspector ..\arcor2_AREditor\Submodules\UnityRuntimeInspector\Plugins
 mklink /j ..\arcor2_AREditor\Assets\Submodules\Unity3DRuntimeTransformGizmo ..\arcor2_AREditor\Submodules\Unity3DRuntimeTransformGizmo\Assets
+mklink /j ..\arcor2_AREditor\Assets\Submodules\RosSharp ..\arcor2_AREditor\Submodules\RosSharp\Unity3D\Assets\RosSharp
 pause
