21360421366660a2fe48a7bc13e42d9d038282b0
xBambusekD <bambusek7619@gmail.com>
2020-05-20 19:44:30 +0200

    Patch for SimpleCollada along with scripts for applying the patch.

---
+++ scripts/patch_simple_collada_asset.bat
@@ -0,0 +1,2 @@
+C:\"Program Files"\UnxUtils\usr\local\wbin\patch.exe ..\arcor2_AREditor\Assets\Submodules\SimpleCollada\ColladaImporter.cs -i ColladaImporter.patch
+pause
---
+++ scripts/patch_simple_collada_asset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+patch ../arcor2_AREditor/Assets/Submodules/SimpleCollada/ColladaImporter.cs -i ColladaImporter.patch
