a9deacc59346df89ca4801d7d1683ff1cc3cd45a
xBambusekD <bambusek7619@gmail.com>
2020-07-08 20:04:43 +0200

    Fix: Default Camera position in Desktop version

--- arcor2_AREditor/Assets/BASE/Scripts/Utils/DefaultCameraPosition.cs
+++ arcor2_AREditor/Assets/BASE/Scripts/Utils/DefaultCameraPosition.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class DefaultCameraPosition : MonoBehaviour {
     
-    public Vector3 InitialPosition = new Vector3(0, 0, 0);
-    public Vector3 InitialRotation = new Vector3(0, 0, 0);
+    public Vector3 InitialPosition = new Vector3(0f, 1.5f, -2f);
+    public Vector3 InitialRotation = new Vector3(45f, 0f, 0f);
 
     private void Start() {
         Camera.main.transform.position = InitialPosition;
