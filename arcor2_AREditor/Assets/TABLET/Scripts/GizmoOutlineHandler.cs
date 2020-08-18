c9042d5783e60176b190ab2c1ac1118078b69835
xBambusekD <bambusek7619@gmail.com>
2020-08-18 14:23:05 +0200

    GizmoOutlineHandler doc comments

--- arcor2_AREditor/Assets/TABLET/Scripts/GizmoOutlineHandler.cs
+++ arcor2_AREditor/Assets/TABLET/Scripts/GizmoOutlineHandler.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The script has to be placed on GameObject manipulated by TransformGizmo.
+/// Clear gizmo will be called when the GameObject is deselected.
+/// </summary>
 public class GizmoOutlineHandler : MonoBehaviour {
 
     public OutlineOnClick OutlineOnClick;
 
+    /// <summary>
+    /// Called via SendMessage("ClearGizmo") from TransformGizmo.cs
+    /// </summary>
     public void ClearGizmo() {
         OutlineOnClick.GizmoUnHighlight();
     }
