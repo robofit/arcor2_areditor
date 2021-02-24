d9f3cf08c58fd2baecffb9c18d9bb60a6a971b19
xBambusekD <bambusek7619@gmail.com>
2021-02-24 13:59:32 +0100

    wip

--- arcor2_AREditor/Assets/BASE/Scripts/WebsocketManager.cs
+++ arcor2_AREditor/Assets/BASE/Scripts/WebsocketManager.cs
@@ -2322,13 +2322,27 @@ namespace Base {
             }
         }
 
-        public async Task<IO.Swagger.Model.Pose> CalibrateTablet(CameraParameters cameraParams, string img) {
+        public async Task<IO.Swagger.Model.Pose> GetCameraPose(CameraParameters cameraParams, string img) {
             int r_id = Interlocked.Increment(ref requestID);
-            IO.Swagger.Model.CalibrationRequestArgs args = new CalibrationRequestArgs(cameraParameters: cameraParams, image: img);
+            IO.Swagger.Model.GetCameraPoseRequestArgs args = new GetCameraPoseRequestArgs(cameraParameters: cameraParams, image: img);
 
-            IO.Swagger.Model.CalibrationRequest request = new IO.Swagger.Model.CalibrationRequest(r_id, "Calibration", args: args);
+            IO.Swagger.Model.GetCameraPoseRequest request = new IO.Swagger.Model.GetCameraPoseRequest(r_id, "GetCameraPose", args: args);
             SendDataToServer(request.ToJson(), r_id, true);
-            IO.Swagger.Model.CalibrationResponse response = await WaitForResult<IO.Swagger.Model.CalibrationResponse>(r_id);
+            IO.Swagger.Model.GetCameraPoseResponse response = await WaitForResult<IO.Swagger.Model.GetCameraPoseResponse>(r_id);
+            if (response == null || !response.Result) {
+                throw new RequestFailedException(response == null ? new List<string>() { "Failed to calibrate tablet" } : response.Messages);
+            } else {
+                return response.Data;
+            }
+        }
+
+        public async Task<List<IO.Swagger.Model.MarkerCorners>> GetMarkerCorners(CameraParameters cameraParams, string img) {
+            int r_id = Interlocked.Increment(ref requestID);
+            IO.Swagger.Model.MarkersCornersRequestArgs args = new MarkersCornersRequestArgs(cameraParameters: cameraParams, image: img);
+
+            IO.Swagger.Model.MarkersCornersRequest request = new IO.Swagger.Model.MarkersCornersRequest(r_id, "MarkersCorners", args: args);
+            SendDataToServer(request.ToJson(), r_id, true);
+            IO.Swagger.Model.MarkersCornersResponse response = await WaitForResult<IO.Swagger.Model.MarkersCornersResponse>(r_id);
             if (response == null || !response.Result) {
                 throw new RequestFailedException(response == null ? new List<string>() { "Failed to calibrate tablet" } : response.Messages);
             } else {
