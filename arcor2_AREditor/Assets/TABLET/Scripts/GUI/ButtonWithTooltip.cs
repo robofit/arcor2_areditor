f79671c94bd471d2c67b791f50fe85aefd42516b
Kapim <michal.kapinus@gmail.com>
2021-03-18 17:56:29 +0100

    fixed yet another robot name vs id

--- arcor2_AREditor/Assets/TABLET/Scripts/GUI/ButtonWithTooltip.cs
+++ arcor2_AREditor/Assets/TABLET/Scripts/GUI/ButtonWithTooltip.cs
@@ -20,6 +20,8 @@ public class ButtonWithTooltip : MonoBehaviour {
 
 
     public void SetInteractivity(bool interactable) {
+        if (Button == null)
+            return;
         Button.interactable = interactable;
         if (interactable) {
             tooltip.DisplayAlternativeDescription = false;
@@ -29,21 +31,29 @@ public class ButtonWithTooltip : MonoBehaviour {
     }
 
     public void SetInteractivity(bool interactable, string alternativeDescription) {
+        if (tooltip == null)
+            return;
         tooltip.DescriptionAlternative = alternativeDescription;
         SetInteractivity(interactable);
         
     }
 
     public void SetDescription(string description) {
+        if (tooltip == null)
+            return;
         tooltip.Description = description;
         tooltip.DisplayAlternativeDescription = false;
     }
 
     public void HideTooltip() {
+        if (TooltipContent == null)
+            return;
         TooltipContent.tooltipAnimator.Play("Out");
     }
 
     public bool IsInteractive() {
+        if (Button == null)
+            return false;
         return Button.interactable;
     }
 
