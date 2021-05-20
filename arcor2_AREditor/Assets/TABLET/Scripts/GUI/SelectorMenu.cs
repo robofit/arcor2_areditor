7d7bbe902332d80b8b64e18efaa86dded971a9ed
Kapim <michal.kapinus@gmail.com>
2021-05-20 14:45:17 +0200

    * feat: only sort alphabet menu when scene / project is fully loaded

--- arcor2_AREditor/Assets/TABLET/Scripts/GUI/SelectorMenu.cs
+++ arcor2_AREditor/Assets/TABLET/Scripts/GUI/SelectorMenu.cs
@@ -371,7 +371,11 @@ public class SelectorMenu : Singleton<SelectorMenu> {
         selectorItem.SetText(interactiveObject.GetName());
         selectorItem.SetObject(interactiveObject, 0, iteration);
         SelectorItems.Add(interactiveObject.GetId(), selectorItem);
-        if (ContentAlphabet.activeSelf)
+        Debug.LogError(GameManager.Instance.GetGameState());
+        Debug.LogError(SceneManager.Instance.Valid);
+        Debug.LogError(ProjectManager.Instance.Valid);
+        if (ContentAlphabet.activeSelf && ((GameManager.Instance.GetGameState() == GameStateEnum.SceneEditor && SceneManager.Instance.Valid) ||
+                                           (GameManager.Instance.GetGameState() == GameStateEnum.ProjectEditor && ProjectManager.Instance.Valid))) // only sort when project / scene is fully loaded
             SwitchToAlphabet(false);
         return selectorItem;
     }
