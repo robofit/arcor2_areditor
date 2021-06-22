b7c7e91ef00126ca8ac1f2f5a6b56f0caab48570
xBambusekD <bambusek7619@gmail.com>
2021-06-22 16:24:34 +0200

    Transform menu wheel is displaying values in set units.

--- arcor2_AREditor/Assets/TABLET/Scripts/GUI/TransformWheelItem.cs
+++ arcor2_AREditor/Assets/TABLET/Scripts/GUI/TransformWheelItem.cs
@@ -7,8 +7,16 @@ public class TransformWheelItem : MonoBehaviour
     public float Value;
     public TMPro.TMP_Text Label;
 
-    public void SetValue(float value) {
+    public void SetValue(float value, float valueUnit) {
         Value = value;
+        SetLabel(valueUnit);
+    }
+
+    public void SetLabel(float value) {
         Label.SetText(value.ToString());
+    }
+
+    public void SetLabel(string value) {
+        Label.SetText(value);
     }
 }
