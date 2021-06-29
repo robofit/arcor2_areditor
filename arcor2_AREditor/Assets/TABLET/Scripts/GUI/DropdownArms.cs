212170251718d87d0da9763dc4b99ada2436178f
Kapim <michal.kapinus@gmail.com>
2021-06-29 10:07:41 +0200

    * added dropdownArms script

---
+++ arcor2_AREditor/Assets/TABLET/Scripts/GUI/DropdownArms.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Michsky.UI.ModernUIPack;
+using Base;
+using UnityEngine.Events;
+using System.Threading.Tasks;
+
+public class DropdonwArms : MonoBehaviour {
+    public DropdownParameter Dropdown;
+
+
+    public async Task Init(string robotId, UnityAction<string> onChangeCallback) {
+        if (!SceneManager.Instance.SceneStarted || string.IsNullOrEmpty(robotId)) {
+            Dropdown.Dropdown.dropdownItems.Clear();
+            gameObject.SetActive(false);
+            return;
+        }
+        try {
+            IRobot robot = SceneManager.Instance.GetRobot(robotId);
+            Dropdown.Dropdown.dropdownItems.Clear();
+            List<string> arms = await robot.GetArmsIds();
+            PutData(arms, onChangeCallback);
+            if (arms.Count == 1)
+                gameObject.SetActive(false);
+        } catch (ItemNotFoundException ex) {
+            Debug.LogError(ex);
+            Base.NotificationsModernUI.Instance.ShowNotification("End effector load failed", "Failed to load end effectors, try again later");
+        }
+
+
+    }
+
+    public void PutData(List<string> data, UnityAction<string> onChangeCallback) {
+        foreach (string ee in data) {
+            CustomDropdown.Item item = new CustomDropdown.Item {
+                itemName = ee
+            };
+            if (onChangeCallback != null) {
+                if (item.OnItemSelection == null) {
+                    item.OnItemSelection = new UnityEvent();
+                }
+                item.OnItemSelection.AddListener(() => onChangeCallback(ee));
+            }
+            Dropdown.Dropdown.dropdownItems.Add(item);
+        }
+        if (Dropdown.Dropdown.dropdownItems.Count > 0) {
+            Dropdown.Dropdown.SetupDropdown();
+            gameObject.SetActive(true);
+        } else {
+            gameObject.SetActive(false);
+        }
+    }
+}
