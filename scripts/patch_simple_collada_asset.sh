ec6c24e22e96270e9d424ace954739b8db9d09e3
Kapim <michal.kapinus@gmail.com>
2021-10-04 12:01:02 +0200

    Added: * patch for UI graph package

--- scripts/patch_simple_collada_asset.sh
+++ scripts/patch_simple_collada_asset.sh
@@ -2,3 +2,4 @@
 
 patch ../arcor2_AREditor/Assets/Submodules/SimpleCollada/ColladaImporter.cs -i ColladaImporter.patch
 patch ../arcor2_AREditor/Assets/Submodules/SimpleCollada/OrbCreationExtensions/StringExtensions.cs -i StringExtensions.patch
+patch ../arcor2_AREditor/Assets/Submodules/SimpleCollada/UIGraph/Scripts/ConnectionPoint.cs -i ConnectionPoint.patch
