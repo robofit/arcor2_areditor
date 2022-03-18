7ae7eab865f379f83dd2a5124cfcd2045ac0d081
ZdenekM <imaterna@fit.vutbr.cz>
2022-03-18 11:01:19 +0100

    feat: third party (paid) assets are now in a private repository

--- scripts/link_submodules.bat
+++ scripts/link_submodules.bat
@@ -2,12 +2,22 @@ md ..\arcor2_AREditor\Assets\Submodules
 mklink /j ..\arcor2_AREditor\Assets\Submodules\JSONObject ..\arcor2_AREditor\Submodules\JSONObject
 mklink /j ..\arcor2_AREditor\Assets\Submodules\UnityDynamicPanels ..\arcor2_AREditor\Submodules\UnityDynamicPanels\Plugins
 mklink /j ..\arcor2_AREditor\Assets\Submodules\UnityRuntimeInspector ..\arcor2_AREditor\Submodules\UnityRuntimeInspector\Plugins
-mklink /j ..\arcor2_AREditor\Assets\Submodules\Unity3DRuntimeTransformGizmo ..\arcor2_AREditor\Submodules\Unity3DRuntimeTransformGizmo\Assets
 mklink /j ..\arcor2_AREditor\Assets\Submodules\RosSharp ..\arcor2_AREditor\Submodules\RosSharp\Unity3D\Assets\RosSharp
 mklink /j ..\arcor2_AREditor\Assets\Submodules\Simple-Side-Menu ..\arcor2_AREditor\Submodules\Simple-Side-Menu
 mklink /j ..\arcor2_AREditor\Assets\Submodules\NativeWebSocket ..\arcor2_AREditor\Submodules\NativeWebSocket\NativeWebSocket\Assets\WebSocket
 mklink /j ..\arcor2_AREditor\Assets\Submodules\Automation ..\arcor2_AREditor\Submodules\trilleon\client\Assets\Automation
 mklink /j ..\arcor2_AREditor\Assets\Submodules\OffScreenIndicator "..\arcor2_AREditor\Submodules\off-screen-indicator\Off Screen Indicator\Assets"
+
+mklink /j "..\arcor2_AREditor\Assets\Submodules\Joystick Pack" "..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\Joystick Pack"
+mklink /j ..\arcor2_AREditor\Assets\Submodules\loadingBar ..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\loadingBar
+mklink /j ..\arcor2_AREditor\Assets\Submodules\LunarConsole ..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\LunarConsole
+mklink /j "..\arcor2_AREditor\Assets\Submodules\Modern UI Pack" "..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\Modern UI Pack"
+mklink /j ..\arcor2_AREditor\Assets\Submodules\plugins ..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\plugins
+mklink /j ..\arcor2_AREditor\Assets\Submodules\SimpleCollada ..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\SimpleCollada
+mklink /j ..\arcor2_AREditor\Assets\Submodules\TriLib ..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\TriLib
+mklink /j ..\arcor2_AREditor\Assets\Submodules\UIGraph ..\arcor2_AREditor\Submodules\arcor2_areditor_private\3rdparty\UIGraph
+
 del ..\arcor2_AREditor\Submodules\RosSharp\Unity3D\Assets\RosSharp\Plugins\External\Newtonsoft.Json.dll*
 del ..\arcor2_AREditor\Submodules\RosSharp\Unity3D\Assets\RosSharp\Plugins\External\Newtonsoft.Json.xml*
+
 pause
