d386f8f70b8dd6eac9afa3e579363f62966e06b8
Kapim <michal.kapinus@gmail.com>
2023-02-14 09:04:34 +0100

    feat: compatibility with project service 1.0.0-rc.4

--- arcor2_AREditor/Assets/BASE/Scripts/MeshImporter.cs
+++ arcor2_AREditor/Assets/BASE/Scripts/MeshImporter.cs
@@ -27,10 +27,10 @@ public class MeshImporter : Singleton<MeshImporter> {
     /// <param name="mesh"></param>
     /// <param name="aoId">ID of action object which is asociated with the mesh</param>
     public void LoadModel(IO.Swagger.Model.Mesh mesh, string aoId) {
-        if (CheckIfNewerRobotModelExists(mesh.Id, mesh.DataId)) {
-            StartCoroutine(DownloadMesh(mesh.Id, mesh.DataId, aoId));
+        if (CheckIfNewerRobotModelExists(mesh.Id, mesh.AssetId)) {
+            StartCoroutine(DownloadMesh(mesh.Id, mesh.AssetId, aoId));
         } else {
-            StartCoroutine(ImportMeshWhenReady(string.Format("{0}/meshes/{1}/{2}", Application.persistentDataPath, mesh.Id, mesh.DataId), aoId, mesh.DataId));
+            StartCoroutine(ImportMeshWhenReady(string.Format("{0}/meshes/{1}/{2}", Application.persistentDataPath, mesh.Id, mesh.AssetId), aoId, mesh.AssetId));
         }
     }
 
